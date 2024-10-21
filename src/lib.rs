1b763cb53e1dfab74bbd2b79fdaa978a98361804
Matthieu Vachon <matt@streamingfast.io>
2024-10-21 14:18:30 -0400

    Added further comment

--- src/lib.rs
+++ src/lib.rs
@@ -17,17 +17,20 @@ const TRANSFER_TOPIC: [u8; 32] =
     hex!("ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef");
 
 // Inferred by looking at `call[2].keccak_preimages` and taking the preimages that specify twice the same slot
+// which is usually the correctly slot. This is dynamic based on the layout of the variables in the Solidity contract
+// so great care must be taken to ensure that the correct slot is used.
+//
+// There is also a way to infer the right slot using heuristics, finding the keccak preimages that are the same
 const MAPPING_SLOT: [u8; 32] =
     hex!("0000000000000000000000000000000000000000000000000000000000000066");
 
 #[substreams::handlers::map]
 fn map_my_data(blk: Block) -> Option<mydata::MyData> {
-    substreams::log::info!("Found block: {:?}", blk.number);
-
     if blk.number != 12130763 {
         return None;
     }
 
+    substreams::log::info!("Found block: {:?}", blk.number);
     for tx in &blk.transaction_traces {
         if tx.hash != TX_HASH {
             continue;
@@ -38,10 +41,19 @@ fn map_my_data(blk: Block) -> Option<mydata::MyData> {
             if log_view.topics.len() == 3 && log_view.topics[0] == TRANSFER_TOPIC {
                 substreams::log::info!("Found transfer, extracting associated balance change");
 
+                // One can infer the MAPPING_SLOT value by iterating on the list of keccak_preimages
+                // and finding 2 preimages that are 64 bytes long, and is of the form `<from>:<slot>`
+                // and `<to>:<slot>` where `<slot>` must be the same.
+                // call_view.call.keccak_preimages.iter().for_each(|preimage| {});
+
                 // Here I used the topic directly because it's an address but encoded over 20 bytes
                 // just like how the storage slot is encoded which is:
                 //
                 // keccak256(32bytes(address) + 32bytes(slot))
+                //
+                // Where `bytes32` ensure that the element is 32 bytes long (padded with zeros if necessary)
+                // and `+` is the concatenation operator so the final address in our example above has
+                // 64 bytes.
                 let from_storage_slot = storage_slot(&log_view.topics[1], &MAPPING_SLOT);
                 let to_storage_slot = storage_slot(&log_view.topics[2], &MAPPING_SLOT);
 
